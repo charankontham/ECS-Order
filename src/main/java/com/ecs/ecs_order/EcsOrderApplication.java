cf7988fb47d1cc9fa322a0ec18fabcb9758bc652
= <=>
2024-09-25 17:15:35 -0400

    Initial commit from local

---
+++ src/main/java/com/ecs/ecs_order/EcsOrderApplication.java
@@ -0,0 +1,13 @@
+package com.ecs.ecs_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsOrderApplication.class, args);
+	}
+
+}
